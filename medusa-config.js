238f8dcd14249d8232cbcda9b5728ef9e76ef1d3
Wempy Aditya Wiryawan <67529099+wempy-aditya@users.noreply.github.com>
2024-09-27 16:10:45 +0700

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -79,12 +79,18 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  redis_url: REDIS_URL
+  // redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig,
+  projectConfig: {
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+  },
   plugins,
   modules,
 };
